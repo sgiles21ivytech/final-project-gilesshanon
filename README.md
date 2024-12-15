d82523e82e5c6dc252372511842afdf42ef133af
Shanon Giles <sgiles21@ivytech.edu>
2024-12-15 16:00:13 -0600

    Modified templates to display Menu items from database

--- README.md
+++
@@ -1,2 +0,0 @@
-# SDEV_220_Final_Project_GilesShanon
-Project designed to create a system that will place online orders for local cafe
--- order/views.py
+++ order/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from.models import MenuItem
 
-# Create your views here.
+def menu_list(request):
+    menu_items = MenuItem.objects.order_by('price')
+    return render(request, 'order/menu_list.html', {'menu_items': menu_items})
+
+
+
+
+
